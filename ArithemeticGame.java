17069ba3761b5b8c9e5ee6638db6ffed2f5f0e50
Colton-Anderson <coltonanderson10@gmail.com>
2021-11-21 16:51:30 -0500

    commit

--- ArithemeticGame.java
+++ ArithemeticGame.java
@@ -3,30 +3,36 @@ import java.util.Scanner;
 public class ArithemeticGame {
 
     private State easyState;
-    private State meadiumState;
+    private State mediumState;
     private State hardState;
     private State state;
     private int score;
     private Scanner reader;
 
     public ArithemeticGame() {
-
+        reader = new Scanner(System.in);
+        easyState = new Easy(this);
+        mediumState = new Medium(this);
+        hardState = new Hard(this);
+        score = 0;
     }
 
     public void pressQuestionButton() {
-
+        if(score > -3 && score < 3) {
+            
+        }
     }
 
     public void setState(State state) {
-
+        this.state = state;
     }
 
     public State getEasyState() {
-        return this.easyState;
+        return easyState;
     }
 
     public State getMediumState() {
-        return this.meadiumState;
+        return this.mediumState;
     }
 
     public State getHardState() {
